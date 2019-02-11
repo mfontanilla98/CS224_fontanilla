43eaf4f0d9058bc6ddaaba13f49a7787619f68a5
Michael Fontanilla <meafontanilla@outlook.com>
2019-02-11 00:44:57 -0800

    completed menu and dictionary

--- Assignment2/hangman.java
+++ Assignment2/hangman.java
@@ -13,7 +13,10 @@ public class hangman
 	public static void menu()
 	{
 		int userDecision;
+
 		outputMenu();
+		userDecision = scan.nextInt();
+		gameSelect(userDecision);
 	}
 
 	public static void outputMenu()
@@ -23,16 +26,40 @@ public class hangman
 		System.out.println("2. Vs. Player");
 		System.out.println("3. Exit Game");
 		System.out.print("\n");
-		System.out.print("Enter the number of your menu choice: ");
+		System.out.print("What game mode would you like to play?: ");
 	}
 
-	public static void gameModes(int userDecision)
+	public static void gameSelect(int userDecision)
 	{
-
+		while(userDecision != 3)
+		{
+			if (userDecision == 1)
+			{
+				vsComputer();
+			}
+			else if (userDecision == 2)
+			{
+				vsPlayer();
+			}
+			else if(userDecision == 3)
+			{
+				break;
+			}
+			else
+			{
+				System.out.println("Invalid input: Please select a game mode");
+			}
+		}
 	}
-	//test HAys addition
-	/*Here's a test*/
-	//hays passback
-	//pass
+
+	public static void vsComputer()
+	{
+	
+	}
+	
+	public static void vsPlayer()
+	{
+	
+	}
 
 }
