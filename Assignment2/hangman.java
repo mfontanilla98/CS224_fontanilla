18ecee74356c3d348927bf9be38decb1c82b4bc8
Ryan Hays <ryanhays02@yahoo.com>
2019-02-11 19:09:34 -0800

    finished vsPlayer and guessingStart functions with intermediate functions to assist guessingStart

--- Assignment2/hangman.java
+++ Assignment2/hangman.java
@@ -25,7 +25,7 @@ public class hangman
 
 		intUserDecision = Integer.parseInt(JOptionPane.showInputDialog("What game mode would you like to play?: "));
 
-		System.out.println(intUserDecision + " nice");
+		//System.out.println(intUserDecision + " nice");
 		gameSelect(intUserDecision, scnr);
 	}
 
@@ -67,7 +67,7 @@ public class hangman
 	public static void vsPlayer(Scanner scnr)
 	{
 		String word = JOptionPane.showInputDialog("Player 1 Enter a word: ");
-		//scnr.nextLine();	//might need to remove. will allegedly catch the endline character
+		word = word.toUpperCase();
 		guessingStart(word, scnr);
 	}
 	public static void guessingStart(String word, Scanner scnr)
@@ -76,33 +76,153 @@ public class hangman
 		int size = word.length();
 		int numErrors = 0;
 		boolean[] correctWord = new boolean[size];
-		System.out.println(correctWord[0]);
-		System.out.println(correctWord[1]);
-		//while(!finished)
-		//{
+
+		while(!finished)
+		{
+			printTree(numErrors, word);
+			checkWordArray(correctWord, size, word);
+
+			//input and capitalize the guess
+			String guess = JOptionPane.showInputDialog("Input a guess: ");
+			char charGuess = guess.charAt(0);
+			charGuess = Character.toUpperCase(charGuess);
+
+			//check if the guess is in the word
 			for (int i = 0; i < size; i++)
 			{
-				if (correctWord[i] = true)
-					numErrors++;
+				if (charGuess == (word.charAt(i)))
+				{
+					correctWord[i] = true;
+				}
 			}
-			if (numErrors > 6)
+
+			//if the guess is not in the word increment numErrors
+			int inWord = word.indexOf(charGuess);
+			if (inWord == -1)
 			{
-				System.out.println("Sorry. You lost.");
-				//break;
+				numErrors++;
 			}
+
+			//when the word is completely guessed, exit game
+			if (checkBoolArray(correctWord) == true)
+			{
+				checkWordArray(correctWord, size, word);
+				System.out.println("Congratulations! You won!");
+				finished = true;
+			}
+		}
+	}
+
+	public static void checkWordArray(boolean[] correctWord, int size, String word)
+	{
+		//empty word to be guessed
+		for (int i = 0; i < size; i ++)
+		{
+			if (correctWord[i] == true)
+				System.out.print(" " + word.charAt(i)+ " ");
+			else
+				System.out.print(" _ ");
+		}
+		System.out.println("\n\n");
+	}
+
+	public static boolean checkBoolArray(boolean[] correctWord)
+	{
+		for (boolean value : correctWord)
+		{
+        if (!value)
+            return false;
+    }
+    return true;
+	}
+
+	public static void printTree(int numErrors, String word)
+	{
+		if (numErrors == 0)
+		{
 			System.out.println(" ___________");
 			System.out.println(" |         |");
 			System.out.println(" |");
 			System.out.println(" |");
 			System.out.println(" |");
 			System.out.println(" |");
+			System.out.println(" |");
 			System.out.println("___");
-
-			for (int i = 0; i < size; i ++)
-			{
-				System.out.print(" _ ");
-			}
-
-		//}
+			System.out.println("Here's how many errors you have: " + numErrors);
+		}
+		else if (numErrors == 1)
+		{
+			System.out.println(" ___________");
+			System.out.println(" |         |");
+			System.out.println(" |         O");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("___");
+			System.out.println("Here's how many errors you have: " + numErrors);
+		}
+		else if (numErrors == 2)
+		{
+			System.out.println(" ___________");
+			System.out.println(" |         |");
+			System.out.println(" |         O");
+			System.out.println(" |         |");
+			System.out.println(" |         |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("___");
+			System.out.println("Here's how many errors you have: " + numErrors);
+		}
+		else if (numErrors == 3)
+		{
+			System.out.println(" ___________");
+			System.out.println(" |         |");
+			System.out.println(" |         O");
+			System.out.println(" |        \\|");
+			System.out.println(" |         |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("___");
+			System.out.println("Here's how many errors you have: " + numErrors);
+		}
+		else if (numErrors == 4)
+		{
+			System.out.println(" ___________");
+			System.out.println(" |         |");
+			System.out.println(" |         O");
+			System.out.println(" |        \\|/");
+			System.out.println(" |         |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("___");
+			System.out.println("Here's how many errors you have: " + numErrors);
+		}
+		else if (numErrors == 5)
+		{
+			System.out.println(" ___________");
+			System.out.println(" |         |");
+			System.out.println(" |         O");
+			System.out.println(" |        \\|/");
+			System.out.println(" |         |");
+			System.out.println(" |        /");
+			System.out.println(" |");
+			System.out.println("___");
+			System.out.println("Here's how many errors you have: " + numErrors);
+		}
+		else if (numErrors == 6)
+		{
+			System.out.println(" ___________");
+			System.out.println(" |         |");
+			System.out.println(" |         O");
+			System.out.println(" |        \\|/");
+			System.out.println(" |         |");
+			System.out.println(" |        / \\");
+			System.out.println(" |");
+			System.out.println("___");
+			System.out.println("Sorry. You lost.");
+			System.out.println("The word was: " + word);
+			System.exit(0);
+		}
 	}
 }
