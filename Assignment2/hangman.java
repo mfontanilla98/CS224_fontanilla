0eb6b7e502d5e3ec7b6d00a813fbff3836758f0c
Ryan Hays <ryanhays02@yahoo.com>
2019-02-11 17:26:50 -0800

    removed infinite loop in gameSelect

--- Assignment2/hangman.java
+++ Assignment2/hangman.java
@@ -2,38 +2,34 @@ import java.util.Scanner;
 import java.util.Random;
 import javax.swing.JOptionPane;
 import java.lang.String;
-import java.util.List;	
+import java.util.List;
 import java.io.IOException;
 
 public class hangman
 {
-	Scanner scan;
+	//Scanner scan = new Scanner(System.in);
 	Scanner CPU_input = new Scanner("dictionary.txt");
 
 	public static void main(String args[])
 	{
-		menu();
-		scan = new Scanner(System.in);
-
+		Scanner scan = new Scanner(System.in);
+		menu(scan);
 	}
 
-	public static void menu()
+	public static void menu(Scanner scnr)
 	{
 		String userDecision;
 		int intUserDecision;
 
-		outputMenu();
+		outputMenu(scnr);
 
-
-
-		
 		intUserDecision = Integer.parseInt(JOptionPane.showInputDialog("What game mode would you like to play?: "));
 
 		System.out.println(intUserDecision + " nice");
-		//gameSelect(userDecision);
+		gameSelect(intUserDecision, scnr);
 	}
 
-	public static void outputMenu()
+	public static void outputMenu(Scanner scnr)
 	{
 		System.out.println("*****MENU*****");
 		System.out.println("1. Vs. Computer");
@@ -42,49 +38,48 @@ public class hangman
 		System.out.print("\n");
 	}
 
-	public static void gameSelect(int userDecision)
+	public static void gameSelect(int userDecision, Scanner scnr)
 	{
-		while(userDecision != 3)
-		{
 			if (userDecision == 1)
 			{
-				vsComputer();
+				vsComputer(scnr);
 			}
 			else if (userDecision == 2)
 			{
-				vsPlayer();
+				vsPlayer(scnr);
 			}
 			else if(userDecision == 3)
 			{
-				break;
+				System.exit(0);
 			}
 			else
 			{
 				System.out.println("Invalid input: Please select a game mode");
+				menu(scnr);
 			}
-		}
 	}
 
-	public static void vsComputer()
+	public static void vsComputer(Scanner scnr)
 	{
-		
+
 	}
 
-	public static void vsPlayer()
+	public static void vsPlayer(Scanner scnr)
 	{
-		String playerWord = JOptionPane.showInputDialog("Player 1 Enter a word: ");
-		String word = scan.nextLine();
-		scan.nextLine();	//might need to remove. will allegedly catch the endline character
-		guessingStart(word);
+		String word = JOptionPane.showInputDialog("Player 1 Enter a word: ");
+		//scnr.nextLine();	//might need to remove. will allegedly catch the endline character
+		guessingStart(word, scnr);
 	}
-	public static void guessingStart(String word)
+	public static void guessingStart(String word, Scanner scnr)
 	{
 		boolean finished = false;
-		size = len(word);
+		int size = word.length();
 		int numErrors = 0;
 		boolean[] correctWord = new boolean[size];
-		while(!finished)
-		{
+		System.out.println(correctWord[0]);
+		System.out.println(correctWord[1]);
+		//while(!finished)
+		//{
 			for (int i = 0; i < size; i++)
 			{
 				if (correctWord[i] = true)
@@ -93,21 +88,21 @@ public class hangman
 			if (numErrors > 6)
 			{
 				System.out.println("Sorry. You lost.");
-				break;
+				//break;
 			}
-			System.println(" ___________");
-			System.println(" |         |");
-			System.println(" |");
-			System.println(" |");
-			System.println(" |");
-			System.println(" |");
-			System.println("___");
+			System.out.println(" ___________");
+			System.out.println(" |         |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println(" |");
+			System.out.println("___");
 
-			for (int i = 0; i < len(word); i ++)
+			for (int i = 0; i < size; i ++)
 			{
-				println(" _ ");
+				System.out.print(" _ ");
 			}
-			
-		}
+
+		//}
 	}
 }
